01a6e37423d53921ef2858ce894b877a49c87252
bhanuprakash <bhanuprakash@gmail.com>
2024-08-10 13:51:38 +0530

    Added Neonnumber

---
+++ src/test/java/swapnumber/Neonnumber.java
@@ -0,0 +1,15 @@
+package swapnumber;
+
+public class Neonnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float P = 10000, R = 5, T = 5;
+
+        // Calculate simple interest
+        float SI = (P * T * R) / 100;
+        System.out.println("Simple interest = " + SI);
+
+	}
+
+}
