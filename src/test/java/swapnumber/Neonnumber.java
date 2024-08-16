5cb68abc0b7358c7ad45fe491b99109c630ee557
bhanu3023 <bhanuprakash80963@gmail.com>
2024-08-16 10:59:07 +0530

    Update Neonnumber.java

--- src/test/java/swapnumber/Neonnumber.java
+++ src/test/java/swapnumber/Neonnumber.java
@@ -4,7 +4,7 @@ public class Neonnumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		float P = 10000, R = 11, T = 10;
+		float P = 100, R = 11, T = 10;
 
         // Calculate simple interest
         float SI = (P * T * R) / 100;
